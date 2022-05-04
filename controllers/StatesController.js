96d63b1d54090e1036d48348236fc29d5d015047
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-05-03 20:39:47 -0700

    contig shows all relevant state. json data

--- controllers/StatesController.js
+++ controllers/StatesController.js
@@ -3,14 +3,22 @@ const data = {
     setStates: function (data) {this.states = data}
 }
 
+//MAKE ME ASYNC!!!
 const getAllStates = (req, res) => {
+    //create array to hold states list
+    let statesList;
+    const contig = req.query?.contig
     //('/states/?contig=true')
-    if (req.query?.contig === 'true') {
+    if (contig === 'true') {
         res.json({"message" : "contig is true"})
     }
     // ('/states/?contig=false')
-    else if (req.query?.contig === 'false'){
-        res.json({"message" : "contig is true"})
+    else if (contig === 'false'){
+        statesList = data.states.filter(st => st.code === 'AK' || st.code === 'HI');
+        // statesList.forEach(state => {
+
+        // })
+        res.json(statesList);
     }
     //if no contig query is specified ('/states')
     else {
