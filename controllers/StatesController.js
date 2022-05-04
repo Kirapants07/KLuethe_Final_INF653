18a3aff0eccf2a428e04e0348820be4c016db951
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-05-04 11:51:42 -0700

    getState is fully functional

--- controllers/StatesController.js
+++ controllers/StatesController.js
@@ -25,12 +25,17 @@ const getAllStates = async (req, res) => {
     else if (contig === 'false'){
         statesList = data.states.filter(st => st.code === 'AK' || st.code === 'HI');
         statesList.forEach(state => {
-            //const stateExists = mongoStates.find(st => st.stateCode === state.code); //CAUSES FATAL ERROR
-            //console.log(stateExists);
-            //if (stateExists) {
-                //let allStateData = [...statesList, ...stateExists.funfacts];
-                //delete allStateData.stateCode;
-            //}
+            try {
+                const stateExists = mongoStates.find(st => st.stateCode === state.code); //CAUSES FATAL ERROR
+                //console.log(stateExists);
+                //if (stateExists) {
+                    //let allStateData = [...statesList, ...stateExists.funfacts];
+                    //delete allStateData.stateCode;
+                //}
+            } catch (err) {
+                console.log("State does not have funfacts.");
+            }
+
          })
         res.json(statesList);
     }
@@ -61,11 +66,17 @@ const getState = async (req, res) => {
     if (!req?.params?.state){
         return res.status(400).json({"message":"Invalid state abbreviation parameter"});
     }
-    const oneState = await mongoStates.findOne({stateCode: req.body.state}).exec();
-    // if (!oneState){
-    //     return res.status(204).json({"message":"Invalid state abbreviation parameter"});
-    // }
-    res.json(oneState);
+    //get info for state from MongoDB
+    const oneMongoState = await mongoStates.findOne({stateCode: req.params.state.toUpperCase()}).exec();
+    //get info for state from JSON data 
+    const oneJSONState = data.states.filter(st => st.code === req.params.state.toUpperCase());
+    let allStateData = oneJSONState[0];
+    try{
+        allStateData.funfacts = oneMongoState.funfacts;
+    } catch (err) {
+        //intentionally empty
+    }
+    res.json(allStateData);
 }
 
 module.exports = {
